4f5e895e53d799af3db98b7d6bd12e4615e41120
nadezda <nadezda@mail.com>
2025-04-14 19:28:21 +0300

    Initial commit

---
+++ src/test/java/lt/projectx/moviewatchlist/MovieWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.projectx.moviewatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
