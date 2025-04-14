1f0f1cfb6da064e71fbed1d980394ac41812f58e
nadezda <nadezda@mail.com>
2025-04-14 19:36:51 +0300

    add entity classes

---
+++ src/main/java/lt/projectx/moviewatchlist/entity/WatchlistEntry.java
@@ -0,0 +1,28 @@
+package lt.projectx.moviewatchlist.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class WatchlistEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    private Movie movie;
+    @ManyToOne
+    private Watcher watcher;
+    private LocalDateTime dateAdded;
+    @Enumerated(EnumType.STRING)
+    private WatchlistStatus status;
+    @Column(precision = 2, scale = 1)
+    private BigDecimal rating;
+}
